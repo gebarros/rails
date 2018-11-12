b9b847210adc6a85f26c0856830e125dcda9b285
gebarros <gebarrosbio@gmail.com>
2018-11-12 16:16:52 -0200

    tests for patient

--- lab/app/models/patient.rb
+++ lab/app/models/patient.rb
@@ -1,8 +1,7 @@
 class Patient < ApplicationRecord
     has_many :exams
-    validates :name, :email, presence: true
-		# validates :name, :email, :phone, :birth_date, :gender, presence: true
+		validates :name, :email, :phone, :birth_date, :gender, presence: true
 		validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-		# validates :phone, format: {with:  /\d[0-9]\)*\z/}
+		validates :phone, format: {with:  /\d[0-9]\)*\z/}
 
 end
--- lab/test/models/patient_test.rb
+++ lab/test/models/patient_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class PatientTest < ActiveSupport::TestCase
   test "Create a valid patient" do
-    patient = Patient.new(name: "Joao Ninguem", email: "joao@bol.br")
+		patient = Patient.new(name: "Joao Ninguem", email: "joao@bol.br", phone: "1198372-7994",
+		birth_date: 13-07-1970, gender: "M")
     assert patient.save
   end
 
@@ -27,6 +28,29 @@ class PatientTest < ActiveSupport::TestCase
     patient = Patient.new(email: "joaoninguem@host.c")
     assert_not patient.save
     assert patient.errors.added? :email, :invalid
-  end
+	end
+	
+	test "No patient phone blank" do
+    patient = Patient.new(phone: "")
+		assert_not patient.save
+    assert patient.errors.added? :phone, :blank
+	end
+	
+	test "Cannot create patient when phone is invalid" do
+    patient = Patient.new(phone: "abcdef")
+		assert_not patient.save
+    assert patient.errors.added? :phone, :invalid
+	end
+	
+	test "No patient birth date blank" do
+    patient = Patient.new(birth_date: "")
+		assert_not patient.save
+    assert patient.errors.added? :birth_date, :blank
+	end
 
+	test "No patient gender blank" do
+    patient = Patient.new(gender: "")
+		assert_not patient.save
+    assert patient.errors.added? :gender, :blank
+	end
 end
