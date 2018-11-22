01c286958f5cce519297475e159f9167188f1e62
gebarros <gebarrosbio@gmail.com>
2018-11-22 11:23:17 -0200

    cria novas rotas

--- lab/config/routes.rb
+++ lab/config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :patients, :exams, :samples
-  
-    root 'patients#index'
-
+  scope module: 'patients' do
+    resources :patients do
+      resources :exams do
+        resources :samples
+      end
+    end
   end
+
+  root 'patients#index'
+
+  resources :patients
+  resources :exams
+  resources :samples
+end
